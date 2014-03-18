0b033683b813afde61b11b25f8f2dca9d1cd97a4
Dimediuk Stas <dymedyuk@gmail.com>
2014-03-18 13:45:18 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChessForThree::Application.config.secret_key_base = '07e742088947c8e23925ea86838a88ff1717a06b33fe4f22cfaa5b063b6ffc09fca6113f98fe165a044f7e71c9ba1ee155a3f6fef10b48f157516b6112ea9546'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ChessForThree::Application.config.session_store :cookie_store, key: '_chess_for_three_session'
