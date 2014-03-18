d416331159da6ac56b7ac2fe065c258c621095d5
Dimediuk Stas <dymedyuk@gmail.com>
2014-03-18 14:52:01 +0200

    Add test home page

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-ChessForThree::Application.config.secret_key_base = '07e742088947c8e23925ea86838a88ff1717a06b33fe4f22cfaa5b063b6ffc09fca6113f98fe165a044f7e71c9ba1ee155a3f6fef10b48f157516b6112ea9546'
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleApp::Application.config.secret_key_base = secure_token
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ ChessForThree::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'static_pages#home'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
