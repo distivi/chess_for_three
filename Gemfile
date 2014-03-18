9ca4e8e9661b9e2c740e2261f6a3cefce662188c
Dimediuk Stas <dymedyuk@gmail.com>
2014-03-18 15:54:51 +0200

    added users controller

--- Gemfile
+++ Gemfile
@@ -32,6 +32,10 @@ group :doc do
   gem 'sdoc', require: false
 end
 
+group :production do
+  gem 'rails_12factor', '0.0.2'
+end
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 ChessForThree::Application.routes.draw do
+  get "users/new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'static_pages#home'
+
+  match '/signup',  to: 'users#new',  via: 'get'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
