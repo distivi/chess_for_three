6dc6726a83a516456cde774d5321a07cb5ceae46
Dimediuk Stas <dymedyuk@gmail.com>
2014-03-24 15:12:27 +0200

    Added room_id col to users table

--- db/migrate/20140322234118_add_users_to_room.rb
+++ db/migrate/20140322234118_add_users_to_room.rb
@@ -1,6 +1,6 @@
 class AddUsersToRoom < ActiveRecord::Migration
   def change
-  	# add_column :users, :room_id, :integer
+  	add_column :users, :room_id, :integer
   	add_index :users, :room_id
   end
 end
